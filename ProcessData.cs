027ca4ccd0ff86bcd5b202576a907339e11f63fb
Kai Cottrell <72996606+kaicottrell@users.noreply.github.com>
2024-10-02 17:11:58 -0600

    Actions<T>, Func<T,TResult>, and Lambda Functions

---
+++ ProcessData.cs
@@ -0,0 +1,22 @@
+﻿public delegate int BizRulesDelegate(int x, int y); 
+
+public class ProcessData
+{ 
+    //Pass me any function that returns an int and takes two ints
+    public void Process(int x, int y, BizRulesDelegate del)
+    {
+        var result = del(x, y);
+        Console.WriteLine(result);
+    }
+    //Pass me a function that returns void and takes two ints
+    public void ProcessAction(int x, int y, Action<int, int> action)
+    {
+        action(x, y);
+        Console.WriteLine("Action has been processed");
+    }
+    public void ProcessFunc(int x, int y, Func<int, int, int> func)
+    {
+        var result = func(x, y);
+        Console.WriteLine(result);
+    }
+}   
--- Program.cs
+++ Program.cs
@@ -3,26 +3,47 @@
 /// Code sourced from the Pluralsight course "C# Events, Delegates, and Lambdas" by Dan Wahlin
 /// </summary>
 
-///<summary>
-/// Event handler for the WorkPerformed event
-/// </summary>
-static void Worker_WorkPerformed(object? sender, WorkPerformedEventArgs e)
+var custs = new List<Customer>
 {
-    Console.WriteLine($"Hours worked: {e.Hours}, Work Type: {e.WorkType}");
-}
-///<summary>
-/// Event handler for the WorkCompleted event
-/// </summary>
-static void Worker_WorkCompleted(object? sender, EventArgs e)
+    new Customer ("Phoenix", "John", "Doe", 1 ),
+    new Customer  ("Phoenix", "John", "Doe", 500 ),
+    new Customer  ("Seattle", "Suki", "Pizzoro", 500 ),
+    new Customer  ("NY City", "Michelle", "Smith", 500 ),
+};
+//Uses lambda expression to filter and order the list
+var phxCusts = custs
+    .Where(c => c.City == "Phoenix" && c.Id < 500)
+    .OrderBy(c => c.FirstName);
+   
+foreach (var cust in phxCusts)
 {
-    Console.WriteLine("Work is done");
+    Console.WriteLine(cust.FirstName);
 }
-
+var data = new ProcessData();
+//Delegates
+BizRulesDelegate addDel = (x, y) => x + y;
+BizRulesDelegate multiplyDel = (x, y) => x * y;
+data.Process(2, 3, addDel);
+data.Process(2, 3, multiplyDel);
+//Actions
+Action<int, int> myAddAction = (x, y) => Console.WriteLine(x + y); 
+Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+data.ProcessAction(2, 3, myAddAction);
+data.ProcessAction(2, 3, myMultiplyAction);
+//Funcs
+Func<int, int, int> addFunc = (x, y) => x + y;  
+Func<int, int, int> multiplyFunc = (x, y) => x * y;
+data.ProcessFunc(3, 2, addFunc);
+data.ProcessFunc(3, 2, multiplyFunc);
 var worker = new Worker();
 //WorkPerformed is the event, EventHandler is the delegate provided by .net, WorkerPerformedEventArgs are our custom values,Worker_WorkPerformed is the event handler
-//Delegate Inference: 
-worker.WorkPerformed += Worker_WorkPerformed;
-worker.WorkCompleted += Worker_WorkCompleted;
+//Lambda example (shorthand method): 
+worker.WorkPerformed += (s, e) =>
+{
+    Console.WriteLine($"Hours worked: {e.Hours}, Work Type: {e.WorkType}");
+    Console.WriteLine("Lambda expression");
+};
+worker.WorkCompleted += (s, e) => Console.WriteLine("Work is Complete");
 worker.DoWork(8, WorkType.GenerateReports);
 public enum WorkType
 {
